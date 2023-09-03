6af2689505659d5481a515359c663af598a20639
Sabryna <97314825+Sabrynascully@users.noreply.github.com>
2023-09-03 16:18:25 +0000

    marcando e desmarcando  checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,14 @@ import { ListItem } from "./components/ListItem";
 const App = () => {
   const [list, setList] = useState<Item[]>([
     { id: 1, name: "Comprar o pão na padaria", done: false },
-    { id: 2, name: "Comprar um bolo na padaria", done: false },
+    { id: 2, name: "Comprar um bolo na padaria", done: true },
   ]);
 
   return (
     <C.Container>
+      <div>
+        <img src="" alt="" />
+      </div>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
         {/*Área de adicionar nova tarefa */}
@@ -19,6 +22,8 @@ const App = () => {
           <ListItem key={index} item={item} />
         ))}
       </C.Area>
+     
+      
     </C.Container>
   );
 };
--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,9 +1,22 @@
 import * as C from "./styles";
-import {} from "../../types/Item";
+import { Item } from "../../types/Item";
+import { useState } from "react";
+
 type Props = {
   item: Item;
 };
 
 export const ListItem = ({ item }: Props) => {
-  return <C.Container>{item.name}</C.Container>;
+  const [isChecked, setIsChecked] = useState(item.done);
+
+  return (
+    <C.Container>
+      <input
+        checked={isChecked}
+        type="checkbox"
+        onChange={(e) => setIsChecked(e.target.checked)}
+      />
+      <label>{item.name}</label>
+    </C.Container>
+  );
 };
